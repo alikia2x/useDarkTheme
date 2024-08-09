54365197113b5a05623a971e6318f16a3b3c79bf
alikia2x <alikia2x@outlook.com>
2024-08-09 14:11:13 +0800

    init: package

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+declare module "@alikia/dark-theme-hook" {
+    /**
+     * A custom React hook to detect the user's preferred color scheme (dark mode).
+     * @returns {boolean} - A boolean indicating whether dark mode is enabled.
+     */
+    function useDarkMode(): boolean;
+
+    export default useDarkMode;
+}
